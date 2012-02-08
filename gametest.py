0390038e12cf90d1b6c69c2819af6b89b3f28201
tom <tom@imakethin.gs>
2012-02-08 09:10:27 +0000

    added a "stop/start" for the sprites to control the underlying camerathread
    added a holding image for unconnected streams

--- gametest.py
+++ gametest.py
@@ -10,15 +10,25 @@ class testsprite(pygame.sprite.Sprite):
 	def __init__(self, pos, url):
 		pygame.sprite.Sprite.__init__(self)
 		self.x, self.y = pos
+		self.url = url
 		self.camera = CameraThread(url[0], url[1])
-		self.stop = True
+		self.stop = False
 		self.camera.start()
 		self.image = pygame.image.load("lol.jpg")
 		self.rect = self.image.get_rect()
 		
 	def stopCamera(self):
+		print "cam stop.."
 		self.stop = True
 		self.camera.kill = True
+		self.camera.join()
+	
+	def startCamera(self):
+		print "cam start.."
+		self.stop = False
+		self.camera.kill = False
+		self.camera = CameraThread(self.url[0], self.url[1])
+		self.camera.start()
 
 	def update(self):
 		pic = self.camera.pic
@@ -26,10 +36,11 @@ class testsprite(pygame.sprite.Sprite):
 			self.image = pygame.transform.scale(pygame.image.frombuffer(pic.tostring(), pic.size, 'RGB'), (160,120))
 
 			self.rect = self.image.get_rect()
+		self.rect.topleft = (self.x, self.y)
 
 
 pygame.init()
-screen = pygame.display.set_mode((800,480))
+screen = pygame.display.set_mode((1000,680))
 running = 1
 spriteGroup = pygame.sprite.Group()
 spriteGroup.add(testsprite((0,0), ("127.0.0.1:9000", "/")))
@@ -40,13 +51,18 @@ while running:
 	for event in pygame.event.get():
 		if event.type == KEYUP and event.key == K_ESCAPE:
 			running = 0
+		elif event.type == KEYUP and event.key == K_a:
+			for s in spriteGroup:
+				if s.stop == True:
+					s.startCamera()
+				else:
+					s.stopCamera()
 		else:
 			pass
 
-	screen.fill((120,0,0))
+	screen.fill((0,0,0))
 	spriteGroup.update()
 	spriteGroup.draw(screen)
-	pygame.gfxdraw.line(screen, 100,100,200,200, (255,255,255))
 	pygame.display.flip()
 
 #kill threads
